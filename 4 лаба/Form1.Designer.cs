d189abe74740ca8c86e8bf695f736af8b0e4a3e8
Даниил Тюрин <tjurin.daniil@rambler.ru>
2022-04-14 17:47:59 +0700

    Добавьте файлы проекта.

---
+++ 4 лаба/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿
+namespace _4_лаба
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Количество столбцов:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(194, 30);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(160, 22);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            this.numericUpDown1.Enter += new System.EventHandler(this.button2_Click);
+            this.numericUpDown1.Leave += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 131);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Матрица А";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(441, 131);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Матрица В";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(896, 131);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Матрица С";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(837, 315);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 36);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "=";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 181);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(368, 326);
+            this.dataGridView1.TabIndex = 18;
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.Click += new System.EventHandler(this.button2_Click);
+            this.dataGridView1.Enter += new System.EventHandler(this.button2_Click);
+            this.dataGridView1.Leave += new System.EventHandler(this.button2_Click);
+            this.dataGridView1.MouseCaptureChanged += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(444, 181);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.Size = new System.Drawing.Size(368, 326);
+            this.dataGridView2.TabIndex = 19;
+            this.dataGridView2.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView2.Click += new System.EventHandler(this.button2_Click);
+            this.dataGridView2.Enter += new System.EventHandler(this.button2_Click);
+            this.dataGridView2.Leave += new System.EventHandler(this.button2_Click);
+            this.dataGridView2.MouseCaptureChanged += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToAddRows = false;
+            this.dataGridView3.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(899, 181);
+            this.dataGridView3.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.ReadOnly = true;
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.Size = new System.Drawing.Size(368, 326);
+            this.dataGridView3.TabIndex = 20;
+            this.dataGridView3.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView3_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1219, 583);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 47);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Выход";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(1181, 48);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 17);
+            this.label8.TabIndex = 22;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(194, 62);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(160, 22);
+            this.numericUpDown2.TabIndex = 9;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            this.numericUpDown2.Enter += new System.EventHandler(this.button2_Click);
+            this.numericUpDown2.Leave += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 62);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Количество строк:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.radioButton1.Location = new System.Drawing.Point(395, 299);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(42, 21);
+            this.radioButton1.TabIndex = 23;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "+";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.button2_Click);
+            this.radioButton1.Click += new System.EventHandler(this.button2_Click);
+            this.radioButton1.Enter += new System.EventHandler(this.button2_Click);
+            this.radioButton1.Leave += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.radioButton2.Location = new System.Drawing.Point(395, 326);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(36, 24);
+            this.radioButton2.TabIndex = 24;
+            this.radioButton2.Text = "-";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.button2_Click);
+            this.radioButton2.Click += new System.EventHandler(this.button2_Click);
+            this.radioButton2.Enter += new System.EventHandler(this.button2_Click);
+            this.radioButton2.Leave += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1404, 645);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Сложение матриц";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ 4 лаба/Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4_лаба
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+
+
+            int a = (int)numericUpDown1.Value;
+
+            if (a <= 10)
+            {
+                dataGridView1.ColumnCount = a;
+                for (int i = 0; i < a; i++)
+                {
+                    dataGridView1.Columns[i].Width = 22;
+                    dataGridView1.Columns[i].HeaderCell.Value = string.Format((i + 1).ToString(), "0");
+                }
+                dataGridView2.ColumnCount = a;
+                for (int i = 0; i < a; i++)
+                {
+                    dataGridView2.Columns[i].Width = 22;
+                    dataGridView2.Columns[i].HeaderCell.Value = string.Format((i + 1).ToString(), "0");
+                }
+                dataGridView3.ColumnCount = a;
+                for (int i = 0; i < a; i++)
+                {
+                    dataGridView3.Columns[i].Width = 22;
+                    dataGridView3.Columns[i].HeaderCell.Value = string.Format((i + 1).ToString(), "0");
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show(
+                                 "Введено неверное значение",
+                                 "Сообщение",
+                                 MessageBoxButtons.OK);
+            }
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            int b;
+            b = (int)numericUpDown2.Value;
+            if (b <= 10)
+            {
+                dataGridView1.RowCount = b;
+                for (int i = 0; i < b; i++)
+                {
+                    dataGridView1.Rows[i].HeaderCell.Value = string.Format((i + 1).ToString(), "0");
+                }
+                dataGridView2.RowCount = b;
+                for (int i = 0; i < b; i++)
+                {
+                    dataGridView2.Rows[i].HeaderCell.Value = string.Format((i + 1).ToString(), "0");
+                }
+                dataGridView3.RowCount = b;
+                for (int i = 0; i < b; i++)
+                {
+                    dataGridView3.Rows[i].HeaderCell.Value = string.Format((i + 1).ToString(), "0");
+                }
+                
+
+            }
+            else
+            {
+                MessageBox.Show(
+                                 "Введено неверное значение",
+                                 "Сообщение",
+                                 MessageBoxButtons.OK);
+            }
+
+            
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            int g = 0;
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                for (int c = 0; c < dataGridView1.Columns.Count; c++)
+                {
+
+                    if (dataGridView1.Rows[i].Cells[c].Value == null)
+                    {
+                        dataGridView1.Rows[i].Cells[c].Value = 0;
+                    }
+                    else
+                    {
+
+                        double l;
+                        string k;
+                        k = dataGridView1.Rows[i].Cells[c].Value.ToString();
+
+                        bool result2 = double.TryParse(k, out l);
+                        if (result2 == true)
+                        {
+                            g++;
+
+
+                        }
+                        else
+                        {
+
+                            label8.Text = "Исходные данные не корректны";
+                        }
+
+                    }
+                }
+            }
+
+            int m = 0;
+            for (int q = 0; q < dataGridView2.Rows.Count; q++)
+            {
+                for (int w = 0; w < dataGridView2.Columns.Count; w++)
+                {
+                    if (dataGridView2.Rows[q].Cells[w].Value == null)
+                    {
+                        dataGridView2.Rows[q].Cells[w].Value = 0;
+                    }
+                    else
+                    {
+                       double f;
+                        string r;
+                        r = dataGridView2.Rows[q].Cells[w].Value.ToString();
+
+                        bool result1 = double.TryParse(r, out f);
+                        if (result1 == true)
+                        {
+                            m++;
+
+                        }
+                        else
+                        {
+                            label8.Text = "Исходные данные не корректны";
+                        }
+                    }
+                }
+            }
+            if (g == m)
+            {
+
+                for (int h = 0; h < dataGridView3.Rows.Count; h++)
+                {
+                    for (int j = 0; j < dataGridView3.Columns.Count; j++)
+                    {
+                        label8.Text = "";
+                        double x, y, z;
+                        x = 0;
+                        y = 0;
+                        z = 0;
+                        x = Convert.ToDouble(dataGridView1.Rows[h].Cells[j].Value);
+                        y = Convert.ToDouble(dataGridView2.Rows[h].Cells[j].Value);
+                        if (radioButton1.Checked)
+                            z = x + y;
+                        else if (radioButton2.Checked)
+                            z = x - y; 
+                        dataGridView3.Rows[h].Cells[j].Value = z;
+                    }
+
+                }
+            }
+        }
+
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void dataGridView3_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            int g = 0;
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                for (int c = 0; c < dataGridView1.Columns.Count; c++)
+                {
+
+                    if (dataGridView1.Rows[i].Cells[c].Value == null)
+                    {
+                        dataGridView1.Rows[i].Cells[c].Value = 0;
+                    }
+                    else
+                    {
+
+                       double l;
+                        string k;
+                        k = dataGridView1.Rows[i].Cells[c].Value.ToString();
+
+                        bool result2 = double.TryParse(k, out l);
+                        if (result2 == true)
+                        {
+                            g++;
+                            
+
+                        }
+                        else
+                        {
+
+                            label8.Text = "Исходные данные не корректны";
+                        }
+
+                    }
+                }
+            }
+
+            int m = 0;
+            for (int q = 0; q < dataGridView2.Rows.Count; q++)
+            {
+                for (int w = 0; w < dataGridView2.Columns.Count; w++)
+                {
+                    if (dataGridView2.Rows[q].Cells[w].Value == null)
+                    {
+                        dataGridView2.Rows[q].Cells[w].Value = 0;
+                    }
+                    else
+                    {
+                        double f;
+                        string r;
+                        r = dataGridView2.Rows[q].Cells[w].Value.ToString();
+
+                        bool result1 = double.TryParse(r, out f);
+                        if (result1 == true)
+                        {
+                            m++;
+                            
+                        }
+                        else
+                        {
+                            label8.Text = "Исходные данные не корректны";
+                        }
+                    }
+                }
+            }
+            if (g == m)
+            {
+
+                for (int h = 0; h < dataGridView3.Rows.Count; h++)
+                {
+                    for (int j = 0; j < dataGridView3.Columns.Count; j++)
+                    {
+                        label8.Text = "";
+                        double x, y, z;
+                        x = 0;
+                        y = 0;
+                        z = 0;
+                        x = Convert.ToDouble(dataGridView1.Rows[h].Cells[j].Value);
+                        y = Convert.ToDouble(dataGridView2.Rows[h].Cells[j].Value);
+                        if (radioButton1.Checked)
+                            z = x + y;
+                        else if (radioButton2.Checked)
+                            z = x - y;
+                        dataGridView3.Rows[h].Cells[j].Value = Convert.ToString(z);
+                    }
+
+                }
+            }
+        }
+
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            dataGridView3.Update();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            dataGridView3.Update();
+        }
+    }
+}
+
